15d1bc045ce0ea2a2a9539583d4b0082cb3d331a
aroy15 <anjonroy57@gmail.com>
2022-11-24 13:40:29 +0600

    laptops API creation done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -10,6 +11,27 @@ const app = express();
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uyqo5y4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const laptopsCollection =  client.db("laptopBuySell").collection("laptops");
+
+        app.get('/laptops', async(req, res)=>{
+            const query = {};
+            const laptops = await laptopsCollection.find(query).toArray();
+            res.send(laptops)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.log(err))
 
 
 app.get('/', (req, res)=>{
