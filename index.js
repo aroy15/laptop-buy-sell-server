da73bee5d5f228ba2200f97e2b8d6761b30310e1
aroy15 <anjonroy57@gmail.com>
2022-11-26 12:28:03 +0600

    booking post mehtod applied

--- index.js
+++ index.js
@@ -16,15 +16,42 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uyqo5y4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 async function run(){
     try{
         const laptopsCollection =  client.db("laptopBuySell").collection("laptops");
+        const categoriesCollection = client.db("laptopBuySell").collection("categories");
+        const bookingsCollection = client.db("laptopBuySell").collection("bookings");
 
-        app.get('/laptops', async(req, res)=>{
-            const query = {};
-            const laptops = await laptopsCollection.find(query).toArray();
-            res.send(laptops)
+        app.get('/categories', async(req, res) => {
+            const query= {}
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories)
         })
+
+        // app.get('/laptops', async(req, res)=>{
+        //     const query = {};
+        //     const laptops = await laptopsCollection.find(query).toArray();
+        //     res.send(laptops)
+        // })
 
         app.get('/laptops/:category', async(req, res)=>{
             const category = req.params.category;
@@ -32,6 +59,25 @@ async function run(){
             const laptopCategory = await laptopsCollection.find(query).toArray();
             res.send(laptopCategory);
         })
+
+        // Post Booking Products
+        app.post('/booking', async(req, res)=>{
+            const data = req.body;
+            const result = await bookingsCollection.insertOne(data);
+            res.send(result);
+        })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+
     }
     finally{
 
@@ -42,7 +88,7 @@ run().catch(err => console.log(err))
 
 
 app.get('/', (req, res)=>{
-    res.send('Laptop By Sell service working')
+    res.send('Laptop Buy Sell service working')
 })
 
 app.listen(port, ()=>{
